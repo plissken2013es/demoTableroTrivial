641b14efbb9536b5720e81b0277b07dac94f8b9c
LuisQuin <plissken2013es@gmail.com>
2017-02-02 13:01:44 +0100

    Added tile uncover effect and mobile support

--- game.js
+++ game.js
@@ -5,7 +5,11 @@ Trivial.Preloader = function () {};
 Trivial.Preloader.prototype = {
     init: function () {
         this.input.maxPointers = 1;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.pageAlignHorizontally = true;
+        //this.scale.pageAlignVertically = true;
+        
+        this.time.desiredFps = 60;
     },
 
     preload: function () {
@@ -13,6 +17,7 @@ Trivial.Preloader.prototype = {
         this.load.image("hero", "astro.png");
         this.load.images(["btn_white", "btn_blue"]);
         this.load.spritesheet('casillas', 'casillas.png', 45, 45);
+        this.load.spritesheet('efecto', 'uncover_tile.png', 45, 45);
     },
 
     create: function () {
@@ -39,6 +44,7 @@ Trivial.Game = function () {
     
     this.dice = null;
     this.boardTiles = null;
+    this.heroLayer = null;
     this.logicBoard = [];
     this.btnBlue = null;
     this.btnWhite = null;
@@ -63,6 +69,7 @@ Trivial.Game.prototype = {
 
         // board
         this.boardTiles = this.add.group();
+        this.heroLayer = this.add.group();
         this.logicBoard = [];
         this.boardList = new Phaser.ArraySet();
         for (var q=0;q<8;q++) {
@@ -109,7 +116,7 @@ Trivial.Game.prototype = {
         console.log(this.heroPos, Math.floor(this.heroPos/7));
         var pos = this.getPosForTile(this.heroPos);
         this.heroTarget = this.heroPos;
-        this.hero = this.add.sprite(pos.x, pos.y, "hero");
+        this.hero = this.add.sprite(pos.x, pos.y, "hero", 0, this.heroLayer);
         this.hero.anchor.setTo(0.5, 1);
         this.game.physics.arcade.enable(this.hero);
         this.hero.enableBody = true;
@@ -121,6 +128,7 @@ Trivial.Game.prototype = {
         // buttons
         this.btnBlue = this.add.button(80, 370, "btn_blue", this.onBtnPress, this);
         this.btnWhite = this.add.button(180, 370, "btn_white", this.onBtnPress, this);
+        console.log(this.btnBlue);
         
         // launch sample dice
         this.dice = this.rnd.between(1, 6);
@@ -135,6 +143,7 @@ Trivial.Game.prototype = {
     
     update: function () {
         this.boardTiles.sort('y', Phaser.Group.SORT_ASCENDING);
+        this.heroLayer.sort('y', Phaser.Group.SORT_ASCENDING);
         
         // to-do: eliminar este bucle tan exigente
         this.boardTiles.forEach(function(t) {
@@ -264,12 +273,33 @@ Trivial.Game.prototype = {
             delete t.blinking;
         }, this);
     },
+    
+    createEffectAt: function(x, y) {
+        var efecto = this.add.sprite(x, y, "efecto", 29, this.heroLayer);
+        efecto.anchor.setTo(0.5, 1);
+        efecto.animations.add("uncover", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], 30);
+        efecto.animations.play("uncover", null, false, true);
+    },
 
-    discoverTile: function(which) {
+    discoverTileAndTweenHero: function(which, tweenHeroDirection) {
         var tileValue = this.BOARD[which];
-        if (tileValue > 0) {
-            this.logicBoard[which].initialFrame = (4+tileValue)*4;
-        }
+        var isCornerTile = tileValue == 0||tileValue == 7||tileValue == 14||tileValue == 21;
+        
+        if (isCornerTile || this.logicBoard[which].initialFrame == (4+tileValue)*4) {
+            this.tweenHero(tweenHeroDirection);
+            return;
+        } else {
+            var pos = this.getPosForTile(which);
+            this.createEffectAt(pos.x, pos.y+10);
+            var uncoverTime = this.time.create(true);
+            uncoverTime.add(500, function() {
+                if (tileValue > 0) {
+                    this.logicBoard[which].initialFrame = (4+tileValue)*4;
+                }                
+                this.tweenHero(tweenHeroDirection);
+            }, this);  
+            uncoverTime.start();
+        }        
     },
     
     doBlinkTiles: function() {
@@ -307,12 +337,12 @@ Trivial.Game.prototype = {
         if (this.heroMoving) return;
         if (btn.key == "btn_blue") {
             this.heroTarget = this.tgt2;
-            this.discoverTile(this.heroTarget);
-            this.tweenHero(false);
+            this.discoverTileAndTweenHero(this.heroTarget, false);
+            //this.tweenHero(false);
         } else if (btn.key == "btn_white") {
             this.heroTarget = this.tgt1;
-            this.discoverTile(this.heroTarget);
-            this.tweenHero(true);
+            this.discoverTileAndTweenHero(this.heroTarget, true);
+            //this.tweenHero(true);
         }
         this.heroMoving = true;
         this.blinkTimer.pause();
@@ -380,7 +410,7 @@ Trivial.Game.prototype = {
     }
 };
 
-var game = new Phaser.Game(360, 640, Phaser.AUTO, 'game');
+var game = new Phaser.Game(360, 640, Phaser.CANVAS, 'game');
 
 game.state.add('Trivial.Preloader', Trivial.Preloader);
 game.state.add('Trivial.Game', Trivial.Game);
